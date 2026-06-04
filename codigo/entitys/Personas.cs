612624fd2b866a792684d5c5b4371f22b295f8c5
Charlie <cmartinheredial22@gmail.com>
2026-06-04 15:15:12 -0300

    se agrego un metodo a la clase padre

--- codigo/entitys/Personas.cs
+++ codigo/entitys/Personas.cs
@@ -4,9 +4,9 @@ abstract class Personas
     private string nombre;
     private string dni;
     int horas_trabajadas;
-    public string Nombre {get {return this.nombre;} set {this.nombre = value;}}
-    public string Dni {get {return this.dni;} set {this.dni = value;}}
-    public int HorasTrabajadas {get {return this.horas_trabajadas;} set {this.horas_trabajadas = value;}}
+    public string Nombre { get { return this.nombre; } set { this.nombre = value; } }
+    public string Dni { get { return this.dni; } set { this.dni = value; } }
+    public int HorasTrabajadas { get { return this.horas_trabajadas; } set { this.horas_trabajadas = value; } }
 
     public Personas(string nombre, string dni, int horas_trabajadas)
     {
@@ -15,6 +15,14 @@ abstract class Personas
         HorasTrabajadas = horas_trabajadas;
     }
 
-    public abstract void MostrarInformacion();  
-   
-} 
+    public abstract void MostrarInformacion();
+
+    public void RegistrarHoras(int cantidad)
+    {
+        if (cantidad > 0)
+        {
+            HorasTrabajadas += cantidad;
+            Console.WriteLine($"\n[Sistema] Se han registrado {cantidad} horas a {Nombre}.");
+        }
+    }
+}
