8d8da3adecc9be7b8dcd26b325b1963fb55ebfc6
Charlie <cmartinheredial22@gmail.com>
2026-06-04 15:34:43 -0300

    se le agregaron los atributos y metodos a cada clase

---
+++ codigo/entitys/Coordinadores.cs
@@ -0,0 +1,23 @@
+using System;
+class Coordinadores : Personas
+{
+    private string areaAsignada;
+    private int personasACargo;
+    public string AreaAsignada { get { return this.areaAsignada; } set { this.areaAsignada = value; } }
+    public int PersonasACargo { get { return this.personasACargo; } set { this.personasACargo = value; } }
+    public Coordinadores(string nombre, string dni, string areaAsignada, int personasACargo)
+        : base(nombre, dni)
+    {
+
+        AreaAsignada = areaAsignada;
+        PersonasACargo = personasACargo;
+    }
+    public override void MostrarInformacion()
+    {
+        Console.WriteLine($" TIPO: Coordinador");
+        Console.WriteLine($" Nombre: {Nombre}");
+        Console.WriteLine($" DNI: {Dni}");
+        Console.WriteLine($" Área Asignada: {AreaAsignada}");
+        Console.WriteLine($" Personal a Cargo: {PersonasACargo} personas");
+    }
+}
--- codigo/entitys/Personas.cs
+++ codigo/entitys/Personas.cs
@@ -3,26 +3,14 @@ abstract class Personas
 {
     private string nombre;
     private string dni;
-    int horas_trabajadas;
     public string Nombre { get { return this.nombre; } set { this.nombre = value; } }
     public string Dni { get { return this.dni; } set { this.dni = value; } }
-    public int HorasTrabajadas { get { return this.horas_trabajadas; } set { this.horas_trabajadas = value; } }
 
-    public Personas(string nombre, string dni, int horas_trabajadas)
+    public Personas(string nombre, string dni)
     {
         Nombre = nombre;
         Dni = dni;
-        HorasTrabajadas = horas_trabajadas;
     }
 
     public abstract void MostrarInformacion();
-
-    public void RegistrarHoras(int cantidad)
-    {
-        if (cantidad > 0)
-        {
-            HorasTrabajadas += cantidad;
-            Console.WriteLine($"\n[Sistema] Se han registrado {cantidad} horas a {Nombre}.");
-        }
-    }
 }
